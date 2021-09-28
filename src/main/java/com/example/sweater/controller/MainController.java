60b3ab1c5a50ab3c0fa4e4809d8c46769ed29bc6
BeVAle <vlad.rotkopf@gmail.com>
2021-09-28 17:50:04 +0500

    4 lesson complete

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.sweater.controller;
 
 import com.example.sweater.entity.Message;
+import com.example.sweater.entity.User;
 import com.example.sweater.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,13 +30,19 @@ public class MainController {
 
         return "main";
     }
+
     @PostMapping("/main")
-    public String add( @RequestParam String text, @RequestParam String tag, Map<String,Object> model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
 
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages",messages);
+        model.put("messages", messages);
 
 
         return "main";
